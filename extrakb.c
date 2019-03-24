41ab0a74f71b95bbf0a396f92a59061efa647f3f
Michał Czarnecki <czarnecky@va.pl>
2019-03-24 12:49:51 +0100

    Modified argument handling.

--- extrakb.c
+++ extrakb.c
@@ -1,3 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2019 Michał Czarnecki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _DEFAULT_SOURCE
+	#define _DEFAULT_SOURCE
+#endif
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -6,6 +34,8 @@
 #include <string.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 #include "keynames.h"
 #include <linux/input.h>
@@ -13,7 +43,7 @@
 typedef struct kstate {
 	struct timeval time;
 	double h;
-	char state; /* 1 = Pressed, 2 = Hold, 0 = Released */
+	char state; /* 1 = Pressed, 2 = Held, 0 = Released */
 } kstate;
 
 double timeval_diff(struct timeval *a, struct timeval *b)
@@ -31,12 +61,22 @@ double timeval_diff(struct timeval *a, struct timeval *b)
 	return D;
 }
 
+void err(const char *fmt, ...)
+{
+	va_list a;
+
+	va_start(a, fmt);
+	vdprintf(2, fmt, a);
+	va_end(a);
+	exit(EXIT_FAILURE);
+}
+
 char* basename(char *S)
 {
 	char *s = S;
 	while (*s) s++;
-	while (s != S && *s != '/') s--;
-	s++;
+	while (S != s && *s != '/') s--;
+	if (*s == '/') s++;
 	return s;
 }
 
@@ -45,85 +85,95 @@ void usage(char *argv0)
 	fprintf(stderr, "Usage: %s [options]...\n", basename(argv0));
 	fprintf(stderr,
 	"Options:\n"
-	"    -i <event file>\n"
-	"    (none)\n"
+	"    -i <event file> Read input from <event file>.\n"
+	"    -h              Display this help message.\n"
 	);
+}
+
+#define NO_ARG do { ++*argv; if (!(mid = **argv)) argv++; } while (0)
+
+char *ARG(char ***argv)
+{
+	char *r = 0;
+
+	(**argv)++;
+	if (***argv) { /* -oARG */
+		r = **argv;
+		(*argv)++;
+	}
+	else { /* -o ARG */
+		(*argv)++;
+		if (**argv && ***argv != '-') {
+			r = **argv;
+			(*argv)++;
+		}
+	}
+	return r;
+}
+
+char* EARG(char ***argv)
+{
+	char *a;
+	a = ARG(argv);
+	if (!a) {
+		err("ERROR: Expected argument.\n");
+	}
+	return a;
 }
 
 int main(int argc, char *argv[])
 {
-	char *argv0, **next_arg, *arg;
+	char *argv0;
 	int ofd = 1, kbe = -1;
+	_Bool mid = 0;
 	ssize_t r;
 	struct input_event E;
-	char *loptn[2], *loptv;
 	kstate S[KEY_MAX];
+	(void)argc;
 
-	argv0 = argv[0];
-	argv++;
-	while (*argv && **argv == '-') {
-		next_arg = argv+1;
-		++*argv;
-		if (**argv == '-') {
-			++*argv;
-			loptn[0] = *argv;
-			while (**argv != '=') ++*argv;
-			loptn[1] = *argv;
-			++*argv;
-			loptv = *argv;
-
-			arg = loptv;
-
-			/* long option */
-			switch (loptn[1]-loptn[0]) {
-			case 5: if (!memcmp("input", loptn[0], 5)) goto input_file;
-			default: break;
-			}
+	argv0 = *argv;
+	++argv;
+	while ((mid && **argv) || (*argv && **argv == '-')) {
+		if (!mid) ++*argv;
+		mid = 0;
+		if ((*argv)[0] == '-' && (*argv)[1] == 0) {
+			argv++;
+			break;
 		}
-		else do {
-			switch (**argv) {
-			case 0:
-				break;
-			case '-':
-				break;
-			case 'i':
-				arg = *next_arg;
-				next_arg++;
-				input_file:
-
-				kbe = open(arg, O_RDONLY);
-				if (kbe == -1) {
-					dprintf(2, "error: '%s': %s\n",
-						arg, strerror(errno));
-					return 1;
-				}
-				break;
-			case 'h':
-				usage(argv0);
-				return 0;
-			default:
-				break;
+		switch (**argv) {
+		case 'i':
+			kbe = open(EARG(&argv), O_RDONLY);
+			if (kbe == -1) {
+				err("open: %s\n", strerror(errno));
+				return 1;
 			}
-			++*argv;
-		} while (**argv);
-		argv = next_arg;
+			break;
+		case 'h':
+			usage(argv0);
+			NO_ARG;
+			return 0;
+		default:
+			usage(argv0);
+			return 1;
+		}
 	}
 
 	memset(S, 0, sizeof(S));
-	while (0 < (r = read(kbe, &E, sizeof(E)))) {
+	while (-1 != (r = read(kbe, &E, sizeof(E)))) {
 		if (E.type != EV_KEY) continue;
 		S[E.code].state = E.value;
 		switch (E.value) {
 		case 0:
 			S[E.code].h = timeval_diff(&S[E.code].time, &E.time);
-			dprintf(ofd, "R %s %.2f\n", keynames[E.code], S[E.code].h);
+			dprintf(ofd, "R %x %s %.3f\n",
+				E.code, keynames[E.code], S[E.code].h);
 			break;
 		case 1:
 			memcpy(&S[E.code].time, &E.time, sizeof(E.time));
-			dprintf(ofd, "P %s\n", keynames[E.code]);
+			dprintf(ofd, "P %x %s\n", E.code, keynames[E.code]);
 			break;
 		case 2:
-			dprintf(ofd, "H %s\n", keynames[E.code]);
+			dprintf(ofd, "H %x %s\n", E.code, keynames[E.code]);
 			break;
 		default:
 			break;
