3c9cdd12ffe8b972df4ae568a1ab1063c7b1561b
Michał Czarnecki <czarnecky@va.pl>
2019-03-24 12:52:37 +0100

    Updated handler.sh to work with new output style.

--- handler.sh
+++ handler.sh
@@ -2,14 +2,19 @@
 while read line
 do
 	t=$(echo "$line" | awk '{print $1}')
-	k=$(echo "$line" | awk '{print $2}')
-	d=$(echo "$line" | awk '{print $3}')
+	h=$(echo "$line" | awk '{print $2}')
+	k=$(echo "$line" | awk '{print $3}')
 	case $t in
-	+)
+	"P")
 		echo "$k"
 		;;
-	-)
+	"R")
+		d=$(echo "$line" | awk '{print $4}')
 		echo "$k $d"
+		;;
+	"H")
+		;;
+	*)
 		;;
 	esac
 done < "${1:-/dev/stdin}"
