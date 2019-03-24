c7829ae034c65a6c5d9a6c17c5d4fd9d31a6907a
Michał Czarnecki <czarnecky@va.pl>
2019-03-24 12:50:50 +0100

    Added LICENSE header.

--- fselect.c
+++ fselect.c
@@ -1,3 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2019 Michał Czarnecki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _DEFAULT_SOURCE
+	#define _DEFAULT_SOURCE
+#endif
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <dirent.h>
@@ -6,6 +34,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+
+#define NUM_FD 1024
 
 char* basename(char *S)
 {
@@ -25,8 +55,6 @@ void usage(char *argv0)
 	"  Sleeps for 1 second to avoid catching user's own input.\n"
 	);
 }
-
-#define NUM_FD 1024
 
 int main(int argc, char *argv[])
 {
